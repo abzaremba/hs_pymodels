3bfb30cce562a19eb73aecf95c32d7ff5fe382de
abzaremba <zaremba.a.a.b@gmail.com>
2024-06-30 17:28:51 +0100

    changing characteristic in the example output changes the recognition of the transphobic message from HS wrt to religion (incorrect) to HS wrt gender identity (correct)!

--- hs6.py
+++ hs6.py
@@ -29,10 +29,12 @@ hs_examples_dict = [
      'example output':['not hate speech','', 1]},
     {'example input':'“Pick one, kidd,” one nudged. Then, “Choose FA GO TT.”',
      'example output':['hate speech','sexual orientation', 1]},
-    {'example input':'Aspie my ass. too slow to remember, so dont waste our time playing',
-     'example output':['hate speech','neurotype', 1]},
+    # {'example input':'Aspie my ass. too slow to remember, so dont waste our time playing',
+    #  'example output':['hate speech','neurotype', 1]},
     {'example input':'“Kept being called a "Jew" for no reason.”; "A player on the other team was named Hitler.” “[One player] threatened to get antisemitic if I didnt talk. [They] talked about Jew tunnels.”',
-     'example output':['hate speech','race', 1]}
+     'example output':['hate speech','race', 1]},
+    {'example input':'if money was grown on trees women would be dating monkeys oh wait never mind',
+     'example output':['hate speech','sex', 1]}
 ]
 
 hs_examples_str = '\n'
@@ -71,7 +73,7 @@ def classify_hs(message:str, protected_characteristics_str:str, HS_definition:st
     3) the probability with two decimal points.
 
     OUTPUT FORMAT:
-    ['hate speech', 'religion', 0.98]
+    ['hate speech', 'sexual orientation', 0.98]
 
     EXAMPLES:
     Consider the following examples:'{examples}'.
